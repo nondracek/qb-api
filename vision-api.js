22a66c5c221e69d7d001c4a37b9b1b5aadcc7b0e
Nikhil Suri <nikhilsuri@comcast.net>
2018-12-21 12:59:09 -0800

    mongoose connection to mongodb up and running

--- vision-api.js
+++ vision-api.js
@@ -1,19 +1,38 @@
-var cool = require('cool-ascii-faces');
+const cool = require('cool-ascii-faces');
 const express = require('express');
+const mongoose = require('mongoose');
+
+const ADMIN_USER = process.env.ADMIN_USER;
+const ADMIN_PASS = process.env.ADMIN_PASS;
+const MLAB_HOST = process.env.MLAB_HOST;
+const MLAB_PORT = process.env.MLAB_PORT;
+const DB_URL = `mongodb://${ADMIN_USER}:${ADMIN_PASS}@${MLAB_HOST}:${MLAB_PORT}/vision`;
+
+const PORT = process.env.PORT || 3000;
 
 // Initialize http server
 const app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
 // Use router for all API endpoints
 // app.use('/', router);
+
+connect()
+  .on('error', console.log)
+  .on('disconnected', connect)
+  .once('open', listen);
 
 app.get('/', function(request, response) {
   response.send(cool());
 });
 
+function listen() {
+  app.listen(PORT, function() {
+    console.log(`Node app is running on port ${PORT}`);
+  });
+}
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+function connect() {
+  const options = { useNewUrlParser: true };
+  mongoose.connect(DB_URL, options);
+  return mongoose.connection;
+}
