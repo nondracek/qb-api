d7d6e74787e584a71810cb447883a5dc744d6b3a
Nikhil Suri <nikhilsuri@comcast.net>
2019-01-31 15:05:28 -0500

    more hacky changes to stitch connection

--- routes/stitch-connect.js
+++
@@ -1,9 +0,0 @@
-const {
-  Stitch,
-  UserPasswordAuthProviderClient
-} = stitch;
-const APP_ID = "marketapp-pgdcl";
-const stitchClient = Stitch.initializeDefaultAppClient(APP_ID);
-const emailPasswordClient = stitchClient.auth
-  .getProviderClient(UserPasswordAuthProviderClient.factory, "userpass");
-
